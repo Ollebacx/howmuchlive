71393905673c864079c3d5056d10daaeb9fa6d76
David <60545359+Ollebacx@users.noreply.github.com>
2020-11-28 18:41:41 +0000

    Mobile first structure and vuetify money working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-app-bar app color="black align-center" dark prominent>
+      
+        <v-img
+          alt="Vuetify Logo"
+          height="140"
+          class="my-5"
+          contain
+          src="https://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/lightning-icon.png"
+        />
+    </v-app-bar>
+
+    <v-main class="yellow">
+      <Calculator />
+    </v-main>
+  <v-btn color="success">text</v-btn>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Calculator from './components/Calculator.vue';
 
 export default {
   name: "App",
+
   components: {
-    HelloWorld
-  }
+    Calculator
+  },
+
+  data: () => ({
+    //
+  })
 };
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.v-input input {
+  font-size: calc(50px + 1vw);
+  max-height: 80px;
+  padding: 5vw;
+}
+.v-input {
+  border-radius: 15px;
+}
+.v-input__slot.white {
+  padding: 10px;
+}
+.v-select__selection {
+  text-align: right;
+  width: 100%;
+  color:white !important;
+  font-weight: 500;
+  font-size: 24px;
+  padding: 5px;
+}
+.v-select input {
+  max-height: 30px;
+  padding: 5px;
+}
+.v-messages__message {
+ font-size: calc(16px + 1vw);
+ padding: 5px;
+ text-align:right;
+ line-height: 16px;
+ direction:ltr;
 }
 </style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,132 @@
+<template>
+  <v-container class="max">
+    <v-row class="text-center">
+      <v-col cols="12">
+        <!-- <v-text-field
+                  label="Amount"
+                  solo
+                  prepend-inner-icon="mdi-wallet-travel"
+                  hint="10.00"
+                  persistent-hint
+                  clearable
+                  @keypress="validate(event)"
+                ></v-text-field> -->
+                <p>How much money does it cost?</p>
+      <vuetify-money
+        class="text-right"
+        v-model="amount"
+        :options="options"
+        :properties="propertiesAmount"
+        :clearable="clearable"
+        placeholder="1200"
+        autofocus=true
+       />
+      </v-col>
+
+        <v-col cols="12" class="mb-0 pb-0">
+          <p>How much is your income?</p>
+        <vuetify-money
+          class="text-right mb-0 pb-0"
+          v-model="salary"
+          :options="options"
+          :properties="propertiesSalary"
+          :clearable="clearable"
+          :placeholder="placeholder"
+        />
+        <v-select
+          id="textComputer"
+          class="purple darken-2 px-3 mt-0 pt-0"
+          :items="items"
+          v-model="salarySelection"
+        ></v-select>
+        </v-col>
+
+        <v-col cols="12" class="mb-0 pb-0">
+          <p>How many hours you work?
+</p>
+        <vuetify-money
+          class="text-right mb-0 pb-0"
+          v-model="salary"
+          :options="options"
+          :properties="propertiesSalary"
+          :clearable="clearable"
+          :placeholder="placeholder"
+        />
+        <v-select
+          id="textComputer"
+          class="purple darken-2 px-3 mt-0 pt-0"
+          :items="items"
+          v-model="salarySelection"
+        ></v-select>
+        </v-col>
+
+  </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Calculator",
+  data: () => ({
+    amount: "",
+    salary:"",
+    salarySelection: "monthly",
+    items: ["yearly","monthly","daily", "hourly"],
+    placeholder: "",
+    readonly: false,
+    disabled: false,
+    outlined: true,
+    clearable: true,
+    valueWhenIsEmpty: "",
+    options: {
+      locale: "fr-FR",
+      prefix: "",
+      suffix: "",
+      length: 9,
+      precision: 0
+    },
+    propertiesAmount: {
+      // hint: "How much money does it cost?",
+      // "persistent-hint":true,
+      "append-icon": "mdi-currency-eur",
+      // solo: true
+      // You can add other v-text-field properties, here.
+    },
+    propertiesSalary: {
+      "append-icon": "mdi-arrow-down-bold-box",
+      // You can add other v-text-field properties, here.
+    },
+  }),
+  methods: {
+    // validate(content) {
+    //   var theEvent = content || window.event;
+
+    //   // Handle paste
+    //   if (theEvent.type === 'paste') {
+    //       key = event.clipboardData.getData('text/plain');
+    //   } else {
+    //   // Handle key press
+    //       var key = theEvent.keyCode || theEvent.which;
+    //       key = String.fromCharCode(key);
+    //   }
+    //   var regex = /[0-9]|\./;
+    //   if( !regex.test(key) ) {
+    //     theEvent.returnValue = false;
+    //     if(theEvent.preventDefault) theEvent.preventDefault();
+    //   }
+    // }
+  },
+};
+</script>
+
+<style scoped>
+.max{
+  max-width: 400px;
+}
+.text-right {
+  direction: rtl;
+}
+#textComputer {
+  font-size: 24px !important;
+}
+</style>
